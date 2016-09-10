16286cc75491c063b6dc25b1cfb6179e6a3c3a2c
gwdgitnom <gwd163nom@163.com>
2016-09-10 17:24:15 +0800

    update image item

--- src/main/java/edu/muc/jxd/item/ImageItem.java
+++ src/main/java/edu/muc/jxd/item/ImageItem.java
@@ -4,5 +4,6 @@ package edu.muc.jxd.item;
  * Created by gwd on 9/10/2016.
  * This is used for a image resource.
  */
-public class ImageItem {
+public class ImageItem<T extends Item<T>> extends Item<T>{
+
 }
